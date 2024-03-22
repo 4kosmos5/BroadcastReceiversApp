e908afb5af161d6260a2936afb8972df341a3f8f
4kosmos5 <s.altushkin@wellsoft.pro>
2024-03-22 16:45:31 +0500

    Finish

--- app/src/main/java/com/semyon/broadcastreceiversapp/MainActivity.kt
+++ app/src/main/java/com/semyon/broadcastreceiversapp/MainActivity.kt
@@ -1,23 +1,59 @@
 package com.semyon.broadcastreceiversapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.semyon.broadcastreceiversapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            if (p1?.action == "loaded") {
+                val percent = p1.getIntExtra("percent", 0)
+                binding.progressBar.progress = percent
+            }
+        }
+
+    }
+
+    private val localBroadcastManager by lazy {
+        LocalBroadcastManager.getInstance(this)
+    }
+
+    private var count = 0
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_LOW)
-        registerReceiver(receiver, intentFilter)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra(MyReceiver.EXTRA_COUNT, count++)
+                localBroadcastManager.sendBroadcast(this)
+            }
+        }
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(MyReceiver.ACTION_CLICKED)
+            addAction("loaded")
+        }
+        localBroadcastManager.registerReceiver(receiver, intentFilter)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
+        localBroadcastManager.unregisterReceiver(receiver)
     }
 }
